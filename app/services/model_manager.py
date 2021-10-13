85989131ca676ee82665a9b5f39df3c78c290b1c
simplg <81249731+simplg@users.noreply.github.com>
2021-10-13 16:20:55 +0200

    change model

--- app/services/model_manager.py
+++ app/services/model_manager.py
@@ -7,7 +7,7 @@ class ModelManager():
         self.__model = None
 
     def init_app(self, app: Flask):
-        self.__model_path = app.config.get("TENSORFLOW_MODEL_PATH", "https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_640x640/1")
+        self.__model_path = app.config.get("TENSORFLOW_MODEL_PATH", "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1")
         self.__model = hub.load(self.__model_path)
 
     @property
